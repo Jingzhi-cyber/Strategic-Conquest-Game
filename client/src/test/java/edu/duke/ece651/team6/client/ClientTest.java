0ad72c1b006bf9e0826f0fbaebae8c66038393f9
jz422 <jz422@duke.edu>
2023-03-22 21:27:29 -0400

    Add testcase for hostname constructor in Client.java and remove useless InetAddress constructor

--- client/src/test/java/edu/duke/ece651/team6/client/ClientTest.java
+++ client/src/test/java/edu/duke/ece651/team6/client/ClientTest.java
@@ -48,61 +48,64 @@ public class ClientTest {
     server.close();
   }
 
-
   // class ClientTestClass {
-  //   Socket getSocket(String localhost, int port) throws UnknownHostException, IOException {
-  //     return new Socket(localhost, port);
-  //   }
+  // Socket getSocket(String localhost, int port) throws UnknownHostException,
+  // IOException {
+  // return new Socket(localhost, port);
+  // }
 
-  //   Socket getSocket(InetAddress addr, int port) throws UnknownHostException, IOException {
-  //     return new Socket(addr, port);
-  //   }
+  // Socket getSocket(InetAddress addr, int port) throws UnknownHostException,
+  // IOException {
+  // return new Socket(addr, port);
+  // }
 
-  //   ObjectOutputStream getServerOutputStream(OutputStream out) throws IOException {
-  //     return new ObjectOutputStream(out);
-  //   }
+  // ObjectOutputStream getServerOutputStream(OutputStream out) throws IOException
+  // {
+  // return new ObjectOutputStream(out);
+  // }
   // }
 
   // @Test
   // void testConstructors() throws UnknownHostException, IOException {
-  //   ClientTestClass clientTest = new ClientTestClass();
-  //   // create a mock object for the Socket class
-  //   // throw a ConnectException when connect() method is called
+  // ClientTestClass clientTest = new ClientTestClass();
+  // // create a mock object for the Socket class
+  // // throw a ConnectException when connect() method is called
 
-  //   when(clientTest.getSocket("localhost", PORT)).thenReturn(clientSocket);
+  // when(clientTest.getSocket("localhost", PORT)).thenReturn(clientSocket);
 
-  //   Socket client = mock(Socket.class);
-  //   when(server.accept()).thenReturn(client);
+  // Socket client = mock(Socket.class);
+  // when(server.accept()).thenReturn(client);
 
-  //   Commit commit = new Commit(1); // replace with your own object
-  //   ByteArrayOutputStream baos = new ByteArrayOutputStream();
-  //   ObjectOutputStream oos = new ObjectOutputStream(baos);
-  //   oos.writeObject(commit);
-  //   oos.flush();
-  //   byte[] serializedObject = baos.toByteArray();
+  // Commit commit = new Commit(1); // replace with your own object
+  // ByteArrayOutputStream baos = new ByteArrayOutputStream();
+  // ObjectOutputStream oos = new ObjectOutputStream(baos);
+  // oos.writeObject(commit);
+  // oos.flush();
+  // byte[] serializedObject = baos.toByteArray();
 
-  //   OutputStream serverOutputStream = mock(OutputStream.class);
-  //   when(client.getOutputStream()).thenReturn(serverOutputStream);
-  //   when(clientTest.getServerOutputStream(serverOutputStream)).thenReturn(oos);
-  //   //when(oos.writeObject(serializedObject);)
-  //   // send the serialized object to the server
-  //   // client.sendObject(serializedObject);
+  // OutputStream serverOutputStream = mock(OutputStream.class);
+  // when(client.getOutputStream()).thenReturn(serverOutputStream);
+  // when(clientTest.getServerOutputStream(serverOutputStream)).thenReturn(oos);
+  // //when(oos.writeObject(serializedObject);)
+  // // send the serialized object to the server
+  // // client.sendObject(serializedObject);
 
-  //   // OutputStream out = clientSocket.getOutputStream();
-  //   // ObjectOutputStream objectOut = new ObjectOutputStream(out);
-  //   // objectOut.writeObject(object);
+  // // OutputStream out = clientSocket.getOutputStream();
+  // // ObjectOutputStream objectOut = new ObjectOutputStream(out);
+  // // objectOut.writeObject(object);
 
-  //   // create a client with the mock socket
-  //   Client newClient = new Client("localhost", PORT);
-  //   assertNotNull(newClient.socket);
+  // // create a client with the mock socket
+  // Client newClient = new Client("localhost", PORT);
+  // assertNotNull(newClient.socket);
   // }
 
   // @Test
-  // void testConstructorWithAddressAndPort() throws IOException, ClassNotFoundException {
-  //   // create a client with address and port
-  //   Client client = new Client(InetAddress.getLocalHost(), PORT);
-  //   // assert that the client socket is not null
-  //   assertNotNull(client.socket);
+  // void testConstructorWithAddressAndPort() throws IOException,
+  // ClassNotFoundException {
+  // // create a client with address and port
+  // Client client = new Client(InetAddress.getLocalHost(), PORT);
+  // // assert that the client socket is not null
+  // assertNotNull(client.socket);
   // }
 
   void setOutputStreamAndAssumption_recvSpecifiedObject(Object object) throws IOException, ClassNotFoundException {
@@ -190,6 +193,47 @@ public class ClientTest {
     client.sendUpdatedGameBasicSetting(new GameBasicSetting(1, 2, null, 3));
 
     client.closeSocket();
+  }
+
+  private class ClientThread implements Runnable {
+    public String hostname;
+    public int port;
+
+    public ClientThread(String hostname, int port) {
+      this.port = port;
+      this.hostname = hostname;
+    }
+
+    @Override
+    public void run() {
+      try {
+        Client client = new Client(hostname, port);
+        String message = (String) client.recvObject();
+        assertEquals(message, "Connected to server successfully!");
+      } catch (IOException | ClassNotFoundException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  @Test
+  public void test_client_constructor() throws IOException, InterruptedException, ClassNotFoundException {
+    ServerSocket serverSocket = new ServerSocket(4444);
+    ClientThread clientThread = new ClientThread("localhost", 4444);
+    Thread connect = new Thread(clientThread);
+    connect.start();
+
+    ClientThread errorHostname = new ClientThread("  ", 4444);
+    Thread errorConnect = new Thread(errorHostname);
+    errorConnect.start();
+
+    Socket connected = serverSocket.accept();
+    OutputStream out = connected.getOutputStream();
+    ObjectOutputStream objectOut = new ObjectOutputStream(out);
+    objectOut.writeObject("Connected to server successfully!");
+
+    connect.join();
+    serverSocket.close();
   }
 
 }
