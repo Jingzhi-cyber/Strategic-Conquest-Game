7bc65d0cc2c8bbd37caf454a6e9d9883555173e9
mw515 <mw515@duke.edu>
2023-04-12 21:13:06 -0400

    controller doc

--- client/src/main/java/edu/duke/ece651/team6/client/controller/Controller.java
+++ client/src/main/java/edu/duke/ece651/team6/client/controller/Controller.java
@@ -1,3 +1,8 @@
+/**
+The Controller class represents a controller for the client side of the application.
+It handles the switching of scenes in the application, shows success and error messages
+to the user, and provides functionality for logging out of the application.
+*/
 package edu.duke.ece651.team6.client.controller;
 
 import java.net.URL;
@@ -14,16 +19,45 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Controller {
+  /**
+   * 
+   * The client object for this controller.
+   */
   protected Client client;
 
+  /**
+   * 
+   * Constructor for the Controller class.
+   * 
+   * @param client The client object for this controller.
+   */
   public Controller(Client client) {
     this.client = client;
   }
 
+  /**
+   * 
+   * Returns the client object for this controller.
+   * 
+   * @return The client object for this controller.
+   */
   public Client getClient() {
     return this.client;
   }
 
+  /**
+   * 
+   * Returns a new scene based on the specified FXML file, CSS file, controllers,
+   * and title.
+   * 
+   * @param url         The URL of the FXML file for the new scene.
+   * @param cssUrl      The URL of the CSS file for the new scene.
+   * @param controllers A HashMap containing the controllers for the new scene.
+   * @param title       The title of the new scene.
+   * @return A new scene based on the specified FXML file, CSS file, controllers,
+   *         and title.
+   * @throws Exception if an error occurs while creating the new scene.
+   */
   public Scene getNewScene(String url, String cssUrl, HashMap<Class<?>, Object> controllers, String title)
       throws Exception {
     URL xmlResource = getClass().getResource(url); // ("/ui/risc-game-main-page.xml");
@@ -64,12 +98,32 @@ public class Controller {
     return scene;
   }
 
+  /**
+   * 
+   * Updates the specified stage with the specified title and scene.
+   * 
+   * @param stage The stage to update.
+   * @param title The title for the updated stage.
+   * @param scene The scene for the updated stage.
+   */
   private void updateScene(Stage stage, String title, Scene scene) {
     stage.setTitle(title);
     stage.setScene(scene);
     stage.show();
   }
 
+  /**
+   * 
+   * Switches to a new page based on the specified FXML file, CSS file,
+   * controllers, and title.
+   * 
+   * @param url         The URL of the FXML file for the new page.
+   * @param cssUrl      The URL of the CSS file for the new page.
+   * @param controllers A HashMap containing the controllers for the new page.
+   * @param title       The title of the new page.
+   * @param cont        The control to use for the new page.
+   * @throws Exception if an error occurs while switching to the new page.
+   */
   public void switchToPage(String url, String cssUrl, HashMap<Class<?>, Object> controllers, String title, Control cont)
       throws Exception {
     Scene scene = getNewScene(url, cssUrl, controllers, title);
@@ -80,6 +134,18 @@ public class Controller {
     updateScene(stage, title, scene);
   }
 
+  /**
+   * 
+   * Switches to a new page based on the specified FXML file, CSS file,
+   * controllers, and title.
+   * 
+   * @param url         The URL of the FXML file for the new page.
+   * @param cssUrl      The URL of the CSS file for the new page.
+   * @param controllers A HashMap containing the controllers for the new page.
+   * @param title       The title of the new page.
+   * @param stage       The stage to use for the new page.
+   * @throws Exception if an error occurs while switching to the new page.
+   */
   public void switchToPage(String url, String cssUrl, HashMap<Class<?>, Object> controllers, String title, Stage stage)
       throws Exception {
 
@@ -89,6 +155,18 @@ public class Controller {
     updateScene(stage, title, scene);
   }
 
+  /**
+   * 
+   * Switches to a new page based on the specified FXML file, CSS file,
+   * controllers, and title.
+   * 
+   * @param url         The URL of the FXML file for the new page.
+   * @param cssUrl      The URL of the CSS file for the new page.
+   * @param controllers A HashMap containing the controllers for the new page.
+   * @param title       The title of the new page.
+   * @param pane        The pane to use for the new page.
+   * @throws Exception if an error occurs while switching to the new page.
+   */
   public void switchToPage(String url, String cssUrl, HashMap<Class<?>, Object> controllers, String title, Pane pane)
       throws Exception {
     Scene scene = getNewScene(url, cssUrl, controllers, title);
@@ -99,6 +177,12 @@ public class Controller {
     updateScene(stage, title, scene);
   }
 
+  /**
+   * 
+   * Displays a success alert to the user with the given message.
+   * 
+   * @param message The message to display in the alert
+   */
   public void showSuccess(String message) {
     Platform.runLater(() -> {
       Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -107,6 +191,12 @@ public class Controller {
     });
   }
 
+  /**
+   * 
+   * Displays an error alert to the user with the given message.
+   * 
+   * @param message The message to display in the alert
+   */
   public void showError(String message) {
     Platform.runLater(() -> {
       Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -115,6 +205,12 @@ public class Controller {
     });
   }
 
+  /**
+   * 
+   * Logs out of the application and switches to the login/register page.
+   * 
+   * @throws Exception If an error occurs while switching pages.
+   */
   protected void logOut() throws Exception {
 
     HashMap<Class<?>, Object> controllers = new HashMap<Class<?>, Object>();
